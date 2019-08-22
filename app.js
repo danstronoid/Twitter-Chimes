a8f8b3723e89cfada6eab09bace6717f0197ca6c
Daniel Schwartz <daniel.schwartz.music@gmail.com>
2019-08-22 10:12:28 -0400

    Added templating engine

--- app.js
+++ app.js
@@ -1,54 +1,58 @@
 require('dotenv').config();
 
+// set up express and requirements
 const express = require('express');
+const nunjucks = require('nunjucks');
 const app = express();
 const path  = require('path');
 const port = process.env.PORT || 3000;
 
 // create twiiter object
 const Twitter = require('twitter');
-var client = require('./tclient.js');
-var stream;
+const client = require('./tclient.js');
+var stream = {};
+var searchKeywords = "";
 
+// configure path for static and views
 app.use('/static', express.static(path.join(__dirname, '/public')));
 app.use(express.static(path.join(__dirname, '/public')));
+app.set('views', './views');
+app.set('view engine', 'nunjucks');
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+});
 
 //app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // the home directory
 app.get('/', (req, res) => {
-
-    res.sendFile(path.join(__dirname + '/public/views/index.html'));
+    res.render('index.html');
 });
 
 // the search form
 app.get('/search', (req, res) => {
-
-    console.log('q: ' + req.query['q']);
-
+    searchKeywords = req.query['q'];
+    console.log('q: ' + searchKeywords);
     res.redirect('/stream');
 });
 
 // start the twitter stream
 app.get('/stream', (req, res) => {
-
-    stream = client.stream('statuses/filter', {track: 'potato'});
+    stream = client.stream('statuses/filter', {track: searchKeywords});
     stream.on('data', function(event) {
         console.log(event && event.text);
     });
     stream.on('error', function(error) {
         throw error;
     });
-
-    res.sendFile(path.join(__dirname + '/public/views/index.html'));
+    res.render('stream.html', {searchKeywords: searchKeywords});
 });
 
 // stop the twitter stream and return to index
 app.get('/stop', (req, res) => {
-
     stream.removeAllListeners();
-
     res.redirect('/');
 });
 
